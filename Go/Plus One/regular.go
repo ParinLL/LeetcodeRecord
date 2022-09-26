4a5f98871eaa914bb5c4dc1639e60f630060a5cb
Parin Lai <parin.lai@rblabs.io>
2022-09-26 23:29:01 +0800

    add: plusOne

---
+++ Go/Plus One/regular.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* Call the function `sayHello` */
+	lists := []int{1, 2, 3}
+	// plusOne(lists)
+	fmt.Println(plusOne(lists))
+
+}
+
+func plusOne(digits []int) []int {
+
+	// looking for last number
+	var last = int(digits[len(digits)-1])
+	if last != 9 {
+		last += 1
+		digits[len(digits)-1] = last
+		return digits
+	}
+
+	newarray := []int{0}
+	// 進位
+	carry := true
+	for i := len(digits) - 2; i >= 0; i-- {
+		// 一般進位
+		if carry == true && digits[i] != 9 {
+			newarray = append(newarray, digits[i]+1)
+			carry = false
+			continue
+		}
+		// 多重進位
+		if carry == true && digits[i] == 9 {
+			newarray = append(newarray, 0)
+			carry = true
+			continue
+		}
+		// 不需進位
+		carry = false
+		newarray = append(newarray, digits[i])
+	}
+	// 當最大一位為9且須進位時值為0，需要在原本的array長度上append 1
+	if newarray[len(digits)-1] == 0 {
+		newarray = append(newarray, 1)
+	}
+
+	// reverse the array
+	for i, j := 0, len(newarray)-1; i < j; i, j = i+1, j-1 {
+		newarray[i], newarray[j] = newarray[j], newarray[i]
+	}
+
+	return newarray
+}
