04b2c5676ccad9f45cf1607353634bfdd102d40a
Parin Lai <parin@shoplineapp.com>
2022-11-15 19:08:36 +0800

    patch: removeElement

--- Go/27. Remove Element/regular.go
+++ Go/27. Remove Element/regular.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	lists := []int{1, 2, 3}
-	fmt.Println(plusOne(lists))
+	fmt.Println(removeElement(lists))
 
 }
 
 func removeElement(nums []int, val int) int {
-    
+
 }
